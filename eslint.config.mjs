0a5ca38d6dc820bc628d350cacde41b60d2f198c
pnodet <paul.nodet@gmail.com>
2025-02-26 15:56:07 +0100

    chore: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import { nivalis } from '@nivalis/eslint-config';
+
+export default nivalis(
+  {
+    tailwindcss: false,
+  },
+  {
+    rules: {
+      'node/no-extraneous-import': 'off',
+    },
+  },
+  {
+    files: ['**/*.spec.ts'],
+    rules: {
+      'no-console': 'off',
+      '@typescript-eslint/no-magic-numbers': 'off',
+      'no-empty-function': 'off',
+    },
+  },
+  {
+    files: ['src/cli/**/*.ts'],
+    rules: {
+      'node/hashbang': 'off',
+      'no-console': 'off',
+    },
+  },
+);
